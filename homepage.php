650388ce378aa2bd3a426198710f62306251b934
aijazbhat <40070943+aijazbhat@users.noreply.github.com>
2018-06-09 01:03:34 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,129 @@
+<html>
+      <head>
+             <title>Home page of my website</title>
+                <style type="text/css">
+				                             
+                                        header tr,td{
+				                                     background-color:000000;
+							                         color:00ff00;
+				
+			                                     	}
+                                        footer{
+				                               height:4cm;
+					                           width:35.3cm;
+				                              background-color:000000;
+					                          font-weight:bolder;
+				                              }
+                </style>
+                                            
+        </head>
+        <body bgcolor="563456">
+              <header role="banner">
+			    <table>
+			         <tr>
+					      <td>
+						            
+                          </td>    
+                          <td align="center" width="1475">						  
+                                                           <h1 align="center">Bhatz Corporation</h1>
+														  <font color="00ff00" size="5"><p>The Future brand</p>
+														<cnter><u><a href="homepage.php">www.bhatz.com</a></u></center></font>
+                           </td>        
+<td>
+</td>						   
+                     </tr>
+                </table>            
+              </header>
+
+           <table>
+		          <tr>
+				  <td>
+						            
+                          </td> 
+				       <td align="center" width="333">
+					          <h2><a href="homepage.php">Home</a></h2>
+					   </td>    
+
+                       <td align="center" width="333">
+					          <h2><a href="homepage.php">Apply</a></h2>
+					   </td> 
+					   
+					   <td align="center" width="333">
+					          <h2><a href="homepage.php">Register</a></h2>
+					   </td> 
+					   
+					   <td align="center" width="333">
+					          <h2><a href="homepage.php">Login</a></h2>
+					   </td> 
+					   <td>
+</td>
+            </table>
+<table>
+<tr>
+<td>
+</td> 
+<td>						  
+<img src="inventory.png"  style="height:200;width:1325;"/> 
+</td>
+<td>
+</td>	
+<tr>
+<td>
+</td>
+<td>
+<img src="success.png"  style="height:200;width:1325;"/> 	
+</td>
+<td>
+</td>	
+</tr>
+<tr>
+<td>
+</td>
+<td>
+<img src="sucess_road.jpg"  style="height:200;width:1325;"/> 
+</td>
+<td>
+</td>
+</tr>
+<tr>
+<td>
+</td>
+<td>
+<img src="road1.jpg"  style="height:200;width:1325;"/> 
+</td>
+<td>
+</td>
+<tr>
+<td>
+</td>
+<td>
+<img src="sunset.jpg"  style="height:200;width:1325;"/> 
+</td>
+<td>
+</td>
+</tr>
+<tr>
+<td>
+</td>
+<td>
+<img src="ocean.jpg"  style="height:200;width:1325;"/> 
+</td>
+<td>
+</td>
+</tr>
+</table>
+<hr>
+          <footer>
+		      <font size="4" color="00ff00">    <center><p>&copy;copyright : Aijaz Ahmad Bhat<br>MscIT Student</p>
+				                    contact : 7006816867,9596583652<br>
+									<form input type="email">e_mail : bhataijaz6009@gmail.com<br>
+									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+									&nbsp;&nbsp;&nbsp;&nbsp;
+									&nbsp;&nbsp;&nbsp;bhataijaz6009@outlook.com</form></font></center>
+          </footer>
+
+
+
+
+</body>
+</html>
